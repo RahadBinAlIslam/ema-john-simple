a692ecb89f820656699983980d0e8033b5a47ee0
Rahad-Bin-Al Islam <rahadbinalislam@gmail.com>
2024-06-08 12:04:13 +0600

    installed firebase

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Orders from "./components/Orders/Orders";
 import About from "./components/About/About";
 import Inventory from "./components/Inventory/Inventory";
 import { productsAndCartLoader } from "./loaders/productsAndCartLoader";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
 
 function App() {
   const router = createBrowserRouter([
@@ -30,6 +32,14 @@ function App() {
         {
           path: "about",
           element: <About></About>,
+        },
+        {
+          path: "login",
+          element: <Login></Login>,
+        },
+        {
+          path: "signup",
+          element: <SignUp></SignUp>,
         },
       ],
     },
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBP6gQtlBMFBLLYhDkzvEAo_Qs-t7owjsM",
+  authDomain: "ema-john-simple-35b23.firebaseapp.com",
+  projectId: "ema-john-simple-35b23",
+  storageBucket: "ema-john-simple-35b23.appspot.com",
+  messagingSenderId: "218918808506",
+  appId: "1:218918808506:web:40078e759ed2ad365b8b83",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
